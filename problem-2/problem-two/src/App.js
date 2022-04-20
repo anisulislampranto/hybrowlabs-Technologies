5c6c1856596e3e3964da82d879cfd4215304aeec
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-21 05:01:02 +0600

    added without delete method

--- problem-2/problem-two/src/App.js
+++ problem-2/problem-two/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
+
+  const handleAdd = async () => {
+    const randomNumber = (await Math.floor(Math.random() * 10)) + 1;
+    const res = await fetch(`https://swapi.dev/api/people/${randomNumber}`);
+    const genaratedData = await res.json();
+    const addedData = [...users, genaratedData];
+    setUsers(addedData);
+  };
+
+  const hadleDelete = (name) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={handleAdd}
+        style={{ marginLeft: "40%", marginTop: "1%" }}
+      >
+        Add Record
+      </button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                {user.name}
+                <button
+                  onClick={() => hadleDelete(user.name)}
+                  style={{ marginLeft: "20px" }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
