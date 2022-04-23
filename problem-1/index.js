be9a5fb7955c29fdd8377d5be1c759c47543625e
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-24 03:09:08 +0600

    done

--- problem-1/index.js
+++ problem-1/index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+let str = "Amolya Sharma";
+let join = str.split(" ").join("");
+let split = join.toUpperCase();
+let obj = {};
+
+for (let i = 0; i < split.length; i++) {
+  if (obj[split[i]] === undefined) {
+    obj[split[i]] = 1;
+  } else {
+    obj[split[i]]++;
+  }
+}
+
+console.log(obj);
